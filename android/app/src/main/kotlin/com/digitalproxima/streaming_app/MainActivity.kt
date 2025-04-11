80c051845a8d709bea7f5be656af154d44c4ed2a
matheus <matheuzdreher@gmail.com>
2025-04-10 22:41:11 -0300

    initial commit

---
+++ android/app/src/main/kotlin/com/digitalproxima/streaming_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.digitalproxima.streaming_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const StreamingApp());
+}
+
+// COLORS: 0xff430027, B13123, CE6627, BFA08E, 300227
+
+class StreamingApp extends StatelessWidget {
+  const StreamingApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      checkerboardOffscreenLayers: false,
+      title: 'Streaming App',
+      home: MainMenu(),
+    );
+  }
+}
+
+class MainMenu extends StatefulWidget {
+  const MainMenu({super.key});
+
+  @override
+  State<MainMenu> createState() => _MainMenuState();
+}
+
+class _MainMenuState extends State<MainMenu> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            title: Text(
+              'Streaming App',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            backgroundColor: Color(0xffB13123),
+            pinned: true,
+            floating: true,
+            expandedHeight: 200,
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) => ContainerCard(index: index),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ContainerCard extends StatefulWidget {
+  const ContainerCard({super.key, required this.index});
+
+  final int index;
+
+  @override
+  State<ContainerCard> createState() => _ContainerCardState();
+}
+
+class _ContainerCardState extends State<ContainerCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        height: 180,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Color(0xffBFA08E),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(left: 10, top: 0),
+          child: Text(widget.index.toString(), style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 25),),
+        ),
+      ),
+    );
+  }
+}
